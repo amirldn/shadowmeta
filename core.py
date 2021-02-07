03a2f15a7e7603432f74d53a5ac925351598fb00
b66957am <amir.maula@student.manchester.ac.uk>
2021-02-07 19:33:09 +0000

    Updated logging info, implemented global polling time, fixed empty name bug

--- core.py
+++ core.py
@@ -1,53 +1,40 @@
 import os
 import time
 import ntpath
-from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
 from watchdog.utils.dirsnapshot import DirectorySnapshot, DirectorySnapshotDiff
-import sys
 import logging
 
-# Exclude .tmp files
-
 shadowplay_dir = "D:\Videos\Shadowplay"
-
+poll_rate = 5
 
 def detectChanges(dirSnapshopRef):
     # hold = input()
-    time.sleep(2)
     dir_snapshot_new = DirectorySnapshot(path=shadowplay_dir, recursive=True)
     list_of_new_files = DirectorySnapshotDiff(ref=dirSnapshopRef, snapshot=dir_snapshot_new).files_created
     if list_of_new_files:
-        logging.info(str(len(list_of_new_files))+" new file(s) found!")
+        logging.info(str(len(list_of_new_files)) + " new file(s) found!")
         for file in list_of_new_files:
             orig_file_name, file_extension = ntpath.splitext(file)
             if file_extension == ".mp4":
                 # print("If you would like to rename it, please type a new name for it else leave it empty.")
-                logging.info(ntpath.basename(file)+" - asking to rename")
+                logging.info(ntpath.basename(file) + " - asking to rename")
                 rename_file(file)
             else:
-                logging.info(ntpath.basename(file)+" was not a .mp4")
-
+                logging.info(ntpath.basename(file) + " was not a .mp4")
     else:
-        logging.info("No new files found, rerunning...")
+        logging.info("No new files found - polling in "+str(poll_rate)+" secs")
     return dir_snapshot_new
 
 
 def rename_file(init_path):
     print("New Shadowplay Found: " + init_path)
-    # print(ntpath.basename(init_path)) #gives filename
-    # print(ntpath.dirname(init_path)) #gives directory name
+    user_new_fn = input("New File Name (leave blank to keep the same)> ")
+    new_path = ntpath.dirname(init_path) + "\\" + user_new_fn
+    new_fn, user_new_file_ext = ntpath.splitext(new_path)
 
-    new_file_name = input("New File Name > ")
-    new_path = ntpath.dirname(init_path) + "\\" + new_file_name
-
-    new_file_name, new_file_extension = ntpath.splitext(new_path)
-    print("New extension: ", new_file_extension)
-
-    if new_file_name:
-        if new_file_extension!=".mp4":
-            new_path = new_path+".mp4"
-
+    if user_new_fn: #if user typed in a file name
+        if user_new_file_ext != ".mp4":
+            new_path = new_path + ".mp4"
         os.rename(init_path, new_path)
         logging.info("File was renamed from " + ntpath.basename(init_path) + " to " + ntpath.basename(new_path))
     else:
@@ -58,33 +45,19 @@ def core():
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    event_handler = LoggingEventHandler()
     execute = True
-    logging.info("Code executed")
+    logging.info("Shadowmeta Launched")
     dir_snapshot_orig = DirectorySnapshot(path=shadowplay_dir, recursive=True)
-    dir_snapshot_curr = detectChanges(dir_snapshot_orig)
+    dir_snapshot_curr = dir_snapshot_orig
     try:
         while execute:
+            time.sleep(poll_rate)
             dir_snapshot_curr = detectChanges(dir_snapshot_curr)
-    except KeyboardInterrupt:
-        execute = False
-        logging.warning("Keyboard Interrupt")
+    except Exception:
+        logging.error(Exception)
     logging.info("End of code reached")
-
 
 core()
 
-# if __name__ == "__main__":
-#     logging.basicConfig(level=logging.INFO,
-#                         format='%(asctime)s - %(message)s',
-#                         datefmt='%Y-%m-%d %H:%M:%S')
-#     event_handler = LoggingEventHandler()
-#     observer = Observer()
-#     observer.schedule(event_handler, path=shadowplay_dir, recursive=True)
-#     observer.start()
-#     try:
-#         while observer.isAlive():
-#             observer.join(1)
-#     except KeyboardInterrupt:
-#         observer.stop()
-#     observer.join()
+# print(ntpath.basename(init_path)) #gives filename
+# print(ntpath.dirname(init_path)) #gives directory name
